17afff54420f2db29d547e1fa8688f855915c498
Mint <dhemfdldid@naver.com>
2023-07-18 23:52:32 +0900

    ROT13
    ROT13

--- 백준/브론즈/1157 : 단어공부.js
+++ 백준/브론즈/1157 : 단어공부.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().trim().split('');
-let inputBigLetterChange = input.map(v => v.toUpperCase())
+let inputBigLetterChange = input.map(v => v.toUpperCase()) //input값 전부 대문자로 변경
 const bigLetter = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 let count = new Array(26).fill(0);
 
-for (let j=0; j<inputBigLetterChange.length; j++) { //대문자 input값
+for (let j=0; j<inputBigLetterChange.length; j++) { //대문자로 바꾼 input값
     for (let i = 0; i<bigLetter.length; i++) { //대문자 배열
         if (bigLetter[i]===(inputBigLetterChange[j])) {
             count[i] += 1;
@@ -12,18 +12,18 @@ for (let j=0; j<inputBigLetterChange.length; j++) { //대문자 input값
     }
 }
 
-const maxValue = Math.max(...count);
+const maxValue = Math.max(...count);//count 배열의 최대값
 
 let maxCountIndex = [];
 
 for (let i = 0; i < count.length; i++) {
-        if (count[i] === maxValue) {
-            maxCountIndex.push(bigLetter[i]);
+        if (count[i] === maxValue) { //count배열을 순회하며 최대값을 만나면
+            maxCountIndex.push(bigLetter[i]); //maxCountIndex배열에 추가
         }
 }
 
 if(maxCountIndex.length === 1 ) {
     console.log(maxCountIndex[0])
-} else {
+} else { //maxCountIndex의 길이가 1이 아니라면 최대값을 가진 인덱스가 많다는 것 or 없는 것
     console.log('?');
 }
